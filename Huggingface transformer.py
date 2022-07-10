ecf686d7d20a232863ccf6f74e53d7ea60e6ffc4
sung jae hyuk <okaybody10@korea.ac.kr>
2022-07-11 02:36:20 +0900

    Data load 코드 완료... 1) bert-kor-base가 분류를 위한 것이 아님 => 찾을 것 2) test set 찾기

--- Huggingface transformer.py
+++ Huggingface transformer.py
@@ -1,6 +1,40 @@
+from sklearn.model_selection import train_test_split
 import torch
 import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader, Dataset, random_split
 
-data = pd.read_csv("ratings_test.txt", sep='\t', engine='python' ,encoding="utf-8", names=["Index", "data", "label"])
+from transformers import BertTokenizer, BertForSequenceClassification
 
-print(data.head(10))
+tokenizer = BertTokenizer.from_pretrained('kykim/bert-kor-base')
+model = BertForSequenceClassification.from_pretrained('kykim/bert-kor-base', num_labels = 2).to('cuda')
+
+# Data Load
+class data(Dataset) :
+    def __init__(self) :
+        data = pd.read_csv("ratings_test.txt", sep='\t', engine='python' ,encoding="utf-8")
+        self.len = data.shape[0]
+        self.comment = data['document']
+        self.labels = data['label']
+    
+    def __getitem__(self, index):
+        return self.comment[index], self.labels[index]
+
+    def __len__(self) :
+        return self.len
+
+dataset = data()
+total_size = len(dataset)
+train_size = int(total_size * 0.8)
+test_size = int(total_size - train_size )
+
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+train_dataloader = DataLoader(train_dataset, batch_size = 64, shuffle = True, drop_last = True)
+test_dataloader = DataLoader(test_dataset, batch_size = 16, shuffle = True, drop_last = True)
+
+# Model
+inputs = tokenizer(["이 영화 너무 재밌는 것 같아요!!", "이 영화 왜 만든거임?"], return_tensors = "pt", padding=True).to('cuda')
+labels = torch.tensor([1, 0]).to('cuda')
+
+outputs = model(**inputs, labels=labels)
